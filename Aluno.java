df73531251d983c115be6a4f310d889bd94071b8
KathlenSouza <kathlensouza60@gmail.com>
2025-06-26 09:53:26 -0300

    ajuste

---
+++ Aluno.java
@@ -0,0 +1,47 @@
+
+public class Aluno {
+	private  String nome;
+	private  String matricula;
+	private  long cpf;
+	private String curso;
+	
+	public Aluno(String nome, String matricula,long cpf,String curso) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.cpf=cpf;
+		this.curso=curso;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+}
